72729b23ed6aa6772261c4046543eab0d1888bdd
cedricahenkorah <cedricahenkorah@gmail.com>
2023-10-12 09:33:47 +0000

    included a function to set(update) the status of the team's subscription to true

--- services/paymentService.js
+++ services/paymentService.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const Payment = require("../models/Payment");
+const Team = require("../models/Team");
 
 class PaymentService {
   async debitWallet(debitData) {
@@ -13,10 +14,20 @@ class PaymentService {
       // save the transaction details to the database
       await this.saveTransaction(debitData);
 
+      // check if the response is successful and update the subscription status of the team
+      if (response.data.success) {
+        await this.updateTeamSubscription(debitData.teamId, true);
+      }
+
       return response.data;
     } catch (error) {
       throw new Error("Payment failed", error.message);
     }
+  }
+
+  async updateTeamSubscription(teamId, isSubscribed) {
+    // update the subscription status of the team in the database
+    await Team.findByIdAndUpdate(teamId, { subscription: isSubscribed });
   }
 
   async saveTransaction(debitData) {
